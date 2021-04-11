b4ac56ace8f7decbe13d0cc5694e2ca07e95673d
Carlos_1007 <carlos.salamanca02@uptc.edu.co>
2021-04-11 14:55:20 -0500

    avances login
    se corrige manejo de informacion por parte del login, se inicia a trabajar en la bienvenida

--- db/schema.rb
+++ db/schema.rb
@@ -12,13 +12,13 @@
 
 ActiveRecord::Schema.define(version: 2021_04_09_031624) do
 
-  create_table "jobs", charset: "utf8", force: :cascade do |t|
+  create_table "jobs", charset: "utf8mb4", force: :cascade do |t|
     t.string "name"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
 
-  create_table "users", charset: "utf8", force: :cascade do |t|
+  create_table "users", charset: "utf8mb4", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
     t.string "reset_password_token"
